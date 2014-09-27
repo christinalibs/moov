32c9cf934d986540517241dd0f4b05ac4df8c388
LOZORD <ljrudberg@gmail.com>
2014-09-27 17:15:54 -0500

    Added false defaults to polymorphic Interests via migration

--- moov/app/models/interest.rb
+++ moov/app/models/interest.rb
@@ -1,14 +1,3 @@
 class Interest < ActiveRecord::Base
-  before_save :set_defaults
   belongs_to :interested, polymorphic: true
-
-  def set_defaults
-    self.farming    ||= false
-    self.fitness    ||= false
-    self.family     ||= false
-    self.civic      ||= false
-    self.arts       ||= false
-    self.tech       ||= false
-    self.education  ||= false
-  end
 end
